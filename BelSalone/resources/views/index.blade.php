74d5621ef6287ba2cd8844797da09ce2c70b8cfa
Manahil Ameer <manahil1054@gmail.com>
2021-01-20 15:04:42 +0500

    Update index.blade.php

--- BelSalone/resources/views/index.blade.php
+++ BelSalone/resources/views/index.blade.php
@@ -217,32 +217,6 @@
               <div id="demo" class="carousel slide" data-ride="carousel">
 
   <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-  
-  <!-- The slideshow -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://source.unsplash.com/TfuheOPlLB4" alt="Los Angeles" width="1100" height="700">
-    </div>
-    <div class="carousel-item">
-      <img src="https://source.unsplash.com/sv0l9N0i7L8" alt="Chicago" width="1100" height="700">
-    </div>
-    <div class="carousel-item">
-      <img src="https://source.unsplash.com/yZIREdPCj3o" alt="New York" width="1100" height="700">
-    </div>
-  </div>
-  
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
 </div>
             </div>
           </div>
@@ -300,14 +274,6 @@
         </div>
 </section>
         <!-- row -->
-        <div class="row">
-          <footer class="col-12">
-            <p class="text-center tm-copyright-text">
-            Copyright 2020 Mini Profile Page 
-            
-            - Design: Template Mo</p>
-          </footer>
-        </div>
       </div>
       <!-- container -->
     </div>
